c1ee37ad8b2898b202c491428856c1f5ac93ae3c
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-14 19:53:23 -0400

    adding

--- client/src/pages/create-recipe.tsx
+++ client/src/pages/create-recipe.tsx
@@ -1,3 +1,58 @@
+import {useState} from "react";
+
 export const CreateRecipe=()=>{
-    return <h1>This is Create Recipe</h1>
+    const [recipe,setRecipe] = useState({
+        name:"",
+        description:"",
+        ingredients:[""],
+        imageUrl:"",
+        cookingTime:0,
+        userOwner:0.
+
+    
+    });
+
+    const handleChange=(event:any)=>{
+        const {name,value} = event?.target;
+        setRecipe({...recipe, [name]:value})
+
+    }
+
+    const addIngredient=()=>{
+        setRecipe({...recipe, ingredients:[...recipe.ingredients,""]});
+    }
+    
+
+
+
+    return <div className="create-recipe" style={{display:"flex  " ,width:"500px"}}>
+        <h2>Create Recipe</h2>
+        <form action="" style={{display:"block"}}>
+            {/* name */}
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+
+            {/* description */}
+            <label htmlFor="description">Description</label>
+            <textarea id="description"  name="description" onChange={handleChange}></textarea>
+            
+            
+            {/* ingredients */}
+            <label htmlFor="ingredients">Ingredients</label>
+            {/* <textarea id="ingredients" name="ingredients"></textarea> */}
+            <button onClick={addIngredient}>Add Ingredients</button>
+
+            {/* instructions */}
+            <label htmlFor="instructions">Instructions</label>
+            <textarea id="instructions" name="instructions" onChange={handleChange}></textarea>
+            
+            {/* imageUrl */}
+            <label htmlFor="imageUrl">Image Url</label>
+            <input type="text" id="imageUrl" name="imageUrl" onChange={handleChange}/>
+
+            {/* cookingTime */}
+            <label htmlFor="cookingTime">Cooking Time (mins)</label>
+            <input type="number" name="cookingTime" id="cookingTime" onChange={handleChange} />
+        </form>
+    </div>
 }
--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -28,5 +28,48 @@ router.post("/", async(req,res)=>{
 
 
 
+// put request is used to add the recipe to the saved.
+router.put("/", async(req,res)=>{
+
+    try{
+        const recipe =await recipeModel.findById(req.body.recipeID)
+        const user = await recipeModel.findById(req.body.userID);
+        user.savedRecipes.push(recipe);
+        await user.save().then(()=>{"Recipe is Saved Successfully"},(err)=>{console.log(err)});
+        res.json({savedRecipes : user.savedRecipes});
+    }
+    catch(err){
+        res.json(err);
+    }
+});
+
+
+// route for showing all the saved recipes 
+router.get("/savedRecipes/ids", async(req,res)=>{
+
+    try{
+    const user = await userModel.findById(req.body.userID);
+    // this is the return that the frontend will recieve
+    res.json({savedRecipes: user?.savedRecipes})
+    }
+    catch(err){
+        res.json(err);
+    }
+})
+
+
+// 
+
+router.get("/savedRecipes", async (req,res)=>{
+    try{
+    const user = await userModel.findByID(req.body.userID)
+    const recipes = await recipeModel.find({_id : {$in : user.savedRecipes}});
+    }
+    catch(err){
+        console.error(err);
+    }
+
+})
+
 
 export {router as recipeRouter}
