b3e6210a2f6e36edd723908f74dcf7a48030bdd0
Muhammad hassan <hassanhanjra900@gmail.com>
2023-08-18 23:52:27 -0400

    update

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,20 @@
 import express from  "express";
+import dotenv from 'dotenv';
 import cors from "cors";
 // mongoose is an ORM for mongodb
 import mongoose from "mongoose";
 import { userRouter } from "./routes/users.js";
 import { recipeRouter } from "./routes/recipes.js";
-
+dotenv.config();
 
 
 let app= express();
 // connecting with mongodb
 // mongoose.connect("mongodb+srv://thedevhassan:891OF4kDWzI4JoY9@recipes.zd1r0oz.mongodb.net/");
-const password= "891OF4kDWzI4JoY9";
-const username='thedevhassan'
+const USERNAME_MONGO = process.env.USERNAME_MONGO;
+const PASSWORD_MONGO = process.env.PASSWORD_MONGO;
+
+
 
 mongoose.connect(`mongodb+srv://${USERNAME_MONGO}:${PASSWORD_MONGO}@recipes.zd1r0oz.mongodb.net/recipe-app?retryWrites=true&w=majority`);
 
